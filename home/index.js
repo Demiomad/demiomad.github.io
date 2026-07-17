174e55431fab00d8c6838a1fbd69d63f2956659d
Demiomad <Demiomad@DESKTOP-0IK5DRT>
2026-07-17 23:12:30 +0200

    refactor(website): complete redesign

--- home/index.js
+++ home/index.js
@@ -8,6 +8,7 @@ const greetings = [
     "Ciao!",
     "Greetings!",
     "こんにちわ！",
+    "안녕하세요！",
     "どうも！",
     "Cześć!",
     "Dzień dobry!",
@@ -16,13 +17,70 @@ const greetings = [
     "你好！",
     "Hoi!",
     "Привет!",
-    "Buenos dias!"
-]
+    "Buenos dias!",
+    "Xin chào!"
+];
+
+const clickSound = new Audio("/resources/audio/click.wav");
+const greetingElement = document.querySelector(".greeting-text");
+const menuButton = document.querySelector(".menu-button");
+const demiomad = document.querySelector(".demiomad");
+const menu = document.querySelector(".menu");
+const counter = document.querySelector(".counter");
+
+let isMenuOpen = false;
+
+function initializeCounter() {
+    let clicks = getClicks();
+    if (clicks <= -1 || isNaN(clicks)) {
+        clicks = 0;
+        localStorage.setItem("clicks", "0");
+    }
+    counter.textContent = clicks.toString();
+    return clicks;
+}
+
+function incrementClicks() {
+    let item = getClicks();
+    if (item <= -1 || isNaN(item)) {
+        localStorage.setItem("clicks", "0");
+        item = 0;
+    }
+
+    const incremented = item + 1;
+    counter.textContent = incremented.toString();
+    localStorage.setItem("clicks", incremented.toString());
+}
+
+function getClicks() {
+    const item = localStorage.getItem("clicks");
+    return parseInt(item);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    const greetingElement = document
-        .querySelector(".greeting");
+    clickSound.load();
 
-    greetingElement.textContent = greetings[Math.floor(
-        Math.random() * greetings.length)];
-})
+    const idx = Math.floor(Math.random() * greetings.length);
+    greetingElement.textContent = greetings[idx] + " 👋";
+
+    initializeCounter();
+});
+
+menuButton.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
+
+    menuButton.src = isMenuOpen ?
+        "/resources/images/open.png" :
+        "/resources/images/closed.png";
+
+    menu.style.opacity = isMenuOpen ? 1 : 0;
+    menu.style.translate = isMenuOpen ? "0 0" : "20px 0"
+    menu.style.pointerEvents = isMenuOpen ? "auto" : "none";
+    menuButton.style.rotate = isMenuOpen ? "-15deg" : "0deg";
+});
+
+demiomad.addEventListener("click", () => {
+    clickSound.currentTime = 0;
+    clickSound.play().catch(() => {});
+    incrementClicks();
+});
